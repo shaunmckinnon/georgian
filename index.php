44636038807fd36dcfca7209e2805a126c05dfe8
Shaun McKinnon <shaun.mckinnon@gshiftlabs.com>
2016-04-19 22:49:44 -0400

    quick index page
    Signed-off-by: Shaun McKinnon <shaun.mckinnon@gshiftlabs.com>

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE HTML>
+<html lang="en">
+
+  <head>
+    <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+    <title>Introduction to Web Programming</title>
+  </head>
+
+  <body>
+    <div class="container">
+
+      <header></header>
+
+      <section></section>
+
+      <footer></footer>
+      
+    </div>
+
+    <script type="text/javascript" src="https://code.jquery.com/jquery-2.2.0.min.js"></script>
+    <script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+  </body>
+  
+</html>
