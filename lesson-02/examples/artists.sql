6a0b09f6f91ad3138bdd5f8d795454929df8b993
Shaun McKinnon <shaun.mckinnon@gshiftlabs.com>
2016-05-06 14:53:00 -0400

    queued lesson files
    Signed-off-by: Shaun McKinnon <shaun.mckinnon@gshiftlabs.com>

--- lesson-02/examples/artists.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE artists (
-  id int(11) NOT NULL AUTO_INCREMENT,
-  name varchar(50) NOT NULL,
-  founded_date date DEFAULT NULL,
-  bio_link varchar(100) DEFAULT NULL,
-  website varchar(100) DEFAULT NULL,
-  PRIMARY KEY (id)
-);
