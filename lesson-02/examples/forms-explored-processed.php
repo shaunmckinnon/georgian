9dfb3af8f56c2f65bf3ab1408e820ef40449d50b
Shaun McKinnon <shaun.mckinnon@gshiftlabs.com>
2016-05-16 11:42:23 -0400

    Added Lesson 02 example files
    Signed-off-by: Shaun McKinnon <shaun.mckinnon@gshiftlabs.com>

--- lesson-02/examples/forms-explored-processed.php
+++ lesson-02/examples/forms-explored-processed.php
@@ -1,35 +1,30 @@
-<?php
-
-  // list all the directories in the project
-  $Directory = new RecursiveDirectoryIterator('./');
-  $Iterator = new RecursiveIteratorIterator($Directory);
-  $Regex = new RegexIterator($Iterator, '/^.+\.php$/i', RecursiveRegexIterator::GET_MATCH);
-
-?>
-
 <!DOCTYPE HTML>
 <html lang="en">
 
   <head>
     <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
     <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
-    <title>Introduction to Web Programming</title>
+    <title>Forms Explored Processed</title>
   </head>
 
   <body>
     <div class="container">
 
       <header>
-        <h1 class="page-header">Introduction to Web Programming</h1>
+        <h1 class="page-header">Forms Explored<small>&nbsp;&mdash;&nbsp;Processed</small></h1>
       </header>
 
       <section>
-        <ul>
-          <?php foreach ($Regex as $name => $object): ?>
-            <li><a href="<?= $name ?>" target="_blank"><?= $name ?></a></li>
-          <?php endforeach ?>
-        </ul>
+        <br><br>
+        <h2>Testing $_POST</h2><hr>
+        <?php
+          echo '<pre>', var_dump($_POST), '</pre>';
+        ?>
       </section>
+
+      <p>
+        <a href="forms-explored.php">Back</a>
+      </p>
       
     </div>
 
