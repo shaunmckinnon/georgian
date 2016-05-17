e27738c96da7d7eb3eadcf486b1d014c21d62eaa
Shaun McKinnon <totheroot@hotmail.ca>
2016-05-17 08:34:34 -0400

    Refactored the code
    Still not very dry, but needed for the varying student needs (Azure, Dreamhost, Localhost, and Heroku)
    
    Signed-off-by: Shaun McKinnon <totheroot@hotmail.ca>

--- shared/connect.php
+++ shared/connect.php
@@ -1,6 +1,8 @@
 <?php
 
-    // get our secrets file ($connection_details)
+    echo $_SERVER['HTTP_HOST'];
+
+    // Localhost or Dreamhost
     if ( file_exists($_SERVER['DOCUMENT_ROOT'] . '/shared/secrets.php') ) {
       // local server
       require_once($_SERVER['DOCUMENT_ROOT'] . '/shared/secrets.php');
@@ -9,30 +11,31 @@
       $dbname = $connection_details['database'];
       $username = $connection_details['username'];
       $password = $connection_details['password'];
-    } else {
+    }
+
+    // Heroku
+    if ( preg_match('/Heroku|georgian\.shaunmckinnon\.ca/i', $_SERVER['HTTP_HOST']) ) {
       // remote server
       $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-      // Heroku
-      if ( $url ) {
-        $host = $url["host"];
-        $dbname = substr($url["path"], 1);
-        $username = $url["user"];
-        $password = $url["pass"];
-      }
+      $host = $url["host"];
+      $dbname = substr($url["path"], 1);
+      $username = $url["user"];
+      $password = $url["pass"];
+    }
+
+    // Azure
+    if ( preg_match('/Azure/i', $_SERVER['HTTP_HOST']) ) {
       $conn_str = getenv("MYSQLCONNSTR_defaultConnection");
-      //Azure
-      if ( $conn_str ) {
-        $parts = explode(';', $conn_str);
-        $url = [];
-        foreach ( $parts as $part ) {
-          $temp = explode('=', $part);
-          $url[$temp[0]] = $temp[1];
-        }
-        $host = $url["Data Source"];
-        $dbname = substr($url["Database"], 1);
-        $username = $url["User Id"];
-        $password = $url["Password"];
+      $parts = explode(';', $conn_str);
+      $url = [];
+      foreach ( $parts as $part ) {
+        $temp = explode('=', $part);
+        $url[$temp[0]] = $temp[1];
       }
+      $host = $url["Data Source"];
+      $dbname = substr($url["Database"], 1);
+      $username = $url["User Id"];
+      $password = $url["Password"];
     }
 
     // connect to our database
