ff27a6ce86736122a36aba940d6bda2e2bb1123d
Shaun McKinnon <totheroot@hotmail.ca>
2016-05-16 23:37:39 -0400

    Adjusted connect.php for Heroku
    Signed-off-by: Shaun McKinnon <totheroot@hotmail.ca>

--- shared/connect.php
+++ shared/connect.php
@@ -1,11 +1,27 @@
 <?php
 
     // get our secrets file ($connection_details)
-    require_once($_SERVER['DOCUMENT_ROOT'] . '/shared/secrets.php');
+    if ( file_exists($_SERVER['DOCUMENT_ROOT'] . '/shared/secrets.php') ) {
+      // local server
+      require_once($_SERVER['DOCUMENT_ROOT'] . '/shared/secrets.php');
+
+      $host = $connection_details['host'];
+      $dbname = $connection_details['database'];
+      $username = $connection_details['username'];
+      $password = $connection_details['password'];
+    } else {
+      // remote server (Heroku)
+      $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+      $host = $url["host"];
+      $dbname = substr($url["path"], 1);
+      $username = $url["user"];
+      $password = $url["pass"];
+    }
 
     // connect to our database
     try {
-      $dbh = new PDO("mysql:host={$connection_details['host']};dbname={$connection_details['database']}", $connection_details['username'], $connection_details['password']);
+      $dbh = new PDO("mysql:host={$host};dbname={$dbname}", $username, $password);
       $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     } catch ( PDOException $e ) {
       echo 'Connection failed: ' . $e->getMessage();
