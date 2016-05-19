57a3635a54c8c2f77c8bd226ccff79562cc0387c
Shaun McKinnon <totheroot@hotmail.ca>
2016-05-19 10:43:43 -0400

    Lesson 02 - Revised
    removed convoluted elements
    
    Signed-off-by: Shaun McKinnon <totheroot@hotmail.ca>

--- lesson-02/examples/add_artist.php
+++ lesson-02/examples/add_artist.php
@@ -1,26 +1,28 @@
 <?php
-
-  // get our connection script
-  require_once( $_SERVER['DOCUMENT_ROOT'] . '/shared/connect.php' );
-
-  // build the SQL statement
-  $sql = 'INSERT INTO artists (name, bio_link) VALUES (:name, :bio_link)';
-
-  // assign our values to variables
-  $name = $_POST['name'];
+  
+  // $_POST to variables
+  $artist_name = $_POST['name'];
   $bio_link = $_POST['bio_link'];
 
-  // prepare the SQL statement
-  $sth = $dbh->prepare( $sql );
+  // connection to database
+  $dbh = new PDO( "mysql:host=localhost;dbname=comp-1006-lesson-examples", "root", "root" );
+  $dbh->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
 
-  // fill the placeholders
-  $sth->bindParam( ':name', $name, PDO::PARAM_STR, 50 );
+  // build the SQL
+  $sql = 'INSERT INTO artists (name, bio_link) VALUES (:name, :bio_link)';
+
+  // prepare our SQL
+  $sth = $dbh->prepare( $sql );
+  $sth->bindParam( ':name', $artist_name, PDO::PARAM_STR, 50 );
   $sth->bindParam( ':bio_link', $bio_link, PDO::PARAM_STR, 100 );
 
   // execute the SQL
   $sth->execute();
 
-  // close the DB connection
+  // close our connection
   $dbh = null;
+
+  // provide confirmation
+  echo "Your artist was saved successfully";
 
 ?>
--- lesson-02/examples/new_artist.php
+++ lesson-02/examples/new_artist.php
@@ -4,35 +4,33 @@
   <head>
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.2/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-aNUYGqSUL9wG/vP7+cWZ5QOM4gsQou3sBfWRr/8S3R1Lv0rysEmnwsRKMbhiQX/O" crossorigin="anonymous">
-    <title>New Artist</title>
+    <title>title</title>
   </head>
 
   <body>
     <!-- This is a Bootstrap container. Get more info at http://getbootstrap.com/ -->
     <div class="container">
-      <h1 class="page-header">Add Artist</h1>
-      
-      <form method="POST" action="add_artist.php">
-        <fieldset>
-          <legend>Artist Information</legend>
-          <div class="form-group">
-            <label>Artist Name</label>
-            <input class="form-control" name='name' placeholder="White Stripes" required>
-          </div>
-          
-          <div class="form-group">
-            <label>Bio Link</label>
-            <input class="form-control" name="bio_link" placeholder="https://en.wikipedia.org/wiki/The_White_Stripes">
-          </div>
+      <h1 class="page-header">Add New Artist</h1>
+      <section>
+        <form action="add_artist.php" method="post">
+          <fieldset>
+            <legend>Artist Information</legend>
+            <div class="form-group">
+              <label for="name">Artist Name</label>
+              <input class="form-control" type="text" name="name">
+            </div>
 
-          <div class="form-group">
-            <button class="btn btn-danger" type="submit">
-              <i class="fa fa-plus"></i>&nbsp;
-              Add Artist
-            </button>
-          </div>
-        </fieldset>
-      </form>
+            <div class="form-group">
+              <label for="bio_link">Bio Link</label>
+              <input class="form-control" type="text" name="bio_link">
+            </div>
+
+            <div>
+              <button class="btn btn-danger">Add Artist</button>
+            </div>
+          </fieldset>
+        </form>
+      </section>
     </div>
     
     <script src="https://code.jquery.com/jquery-2.2.3.min.js" integrity="sha256-a23g1Nt4dtEYOj7bR+vTu7+T8VP13humZFBJNIYoEJo=" crossorigin="anonymous"></script>
