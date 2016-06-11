94db2ad6f39de238ca7f4acf9be555fd2679c2a6
Shaun McKinnon <totheroot@hotmail.ca>
2016-06-11 14:04:09 -0400

    Updated the helper with a links page
    Signed-off-by: Shaun McKinnon <totheroot@hotmail.ca>

---
+++ php_quick_help/php-help-quick-links.php
@@ -0,0 +1,118 @@
+<!DOCTYPE>
+<html>
+  <head>
+    <link crossorigin='anonymous' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css' integrity='sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7' rel='stylesheet'>
+    <link crossorigin='anonymous' href='https://maxcdn.bootstrapcdn.com/font-awesome/4.6.2/css/font-awesome.min.css' integrity='sha384-aNUYGqSUL9wG/vP7+cWZ5QOM4gsQou3sBfWRr/8S3R1Lv0rysEmnwsRKMbhiQX/O' rel='stylesheet'>
+    <title>PHP Doc Quick Help Links</title>
+  </head>
+
+  <body>
+    <h1 class="page-header">COMP-1006 Helpful Links</h1>
+    <div class="panel panel-default">
+      <div class="panel-heading">
+        <h2 class="panel-title">Shaun's Über Custom Helpful Files</h2>
+      </div>
+
+      <div class="panel-body">
+        <ul>
+          <li><a target="_blank" href="https://github.com/shaunmckinnon/georgian/tree/master/php_quick_help">PHP Quick Help Files</a></li>
+          <li><a target="_blank" href="https://raw.githubusercontent.com/shaunmckinnon/georgian/master/php_quick_help/template.php">HTML Template File (RAW)</a></li>
+        </ul>
+      </div>
+    </div>
+
+    <div class="panel panel-default">
+      <div class="panel-heading">
+        <h2 class="panel-title">Database Help Links</h2>
+      </div>
+
+      <div class="panel-body">
+        <ul>
+          <li><a target="_blank" href="http://php.net/manual/en/pdo.connections.php">Connecting to a Database With PDO</a></li>
+          <li><a target="_blank" href="http://php.net/manual/en/pdo.error-handling.php">PDO Error Handling</a></li>
+          <li><a target="_blank" href="http://php.net/manual/en/pdo.prepare.php">PDO prepare()</a></li>
+          <li><a target="_blank" href="http://php.net/manual/en/pdostatement.bindparam.php">PDO bindParam()</a></li>
+          <li><a target="_blank" href="http://php.net/manual/en/pdostatement.execute.php">PDO execute()</a></li>
+          <li><a target="_blank" href="http://php.net/manual/en/pdostatement.fetchall.php">PDO fetchAll()</a></li>
+          <li><a target="_blank" href="http://php.net/manual/en/pdostatement.fetch.php">PDO fetch()</a></li>
+        </ul>
+      </div>
+    </div>
+
+    <div class="panel panel-default">
+      <div class="panel-heading">
+        <h2 class="panel-title">Form Help</h2>
+      </div>
+
+      <div class="panel-body">
+        <ul>
+          <li><a target="_blank" href="https://developer.mozilla.org/en-US/docs/Web/Guide/HTML/Forms">HTML Forms</a></li>
+          <li><a target="_blank" href="http://php.net/manual/en/reserved.variables.post.php">PHP $_POST</a></li>
+          <li><a target="_blank" href="http://php.net/manual/en/reserved.variables.get.php">PHP $_GET</a></li>
+        </ul>
+      </div>
+    </div>
+
+    <div class="panel panel-default">
+      <div class="panel-heading">
+        <h2 class="panel-title">MySQL Help Links</h2>
+      </div>
+
+      <div class="panel-body">
+        <ul>
+          <li><a target="_blank" href="http://dev.mysql.com/doc/refman/5.7/en/insert.html">INSERT</a></li>
+          <li><a target="_blank" href="http://dev.mysql.com/doc/refman/5.7/en/select.html">SELECT</a></li>
+          <li><a target="_blank" href="http://dev.mysql.com/doc/refman/5.7/en/update.html">UPDATE</a></li>
+          <li><a target="_blank" href="http://dev.mysql.com/doc/refman/5.7/en/delete.html">DELETE</a></li>
+        </ul>
+      </div>
+    </div>
+
+    <div class="panel panel-default">
+      <div class="panel-heading">
+        <h2 class="panel-title">PHP Language Help Links</h2>
+      </div>
+
+      <div class="panel-body">
+        <ul>
+          <li><a target="_blank" href="http://php.net/manual/en/language.basic-syntax.comments.php">PHP Commenting</a></li>
+          <li><a target="_blank" href="http://php.net/manual/en/function.echo.php">PHP echo()</a></li>
+          <li><a target="_blank" href="http://php.net/manual/en/control-structures.foreach.php">PHP foreach() Loop</a></li>
+        </ul>
+      </div>
+    </div>
+
+    <div class="panel panel-default">
+      <div class="panel-heading">
+        <h2 class="panel-title">PHP Validation</h2>
+      </div>
+
+      <div class="panel-body">
+        <ul>
+          <li><a target="_blank" href="http://php.net/manual/en/language.operators.comparison.php">PHP Comparison Operators</a></li>
+          <li><a target="_blank" href="http://php.net/manual/en/function.empty.php">PHP empty()</a></li>
+          <li>
+            <a target="_blank" href="http://php.net/manual/en/function.filter-var.php">PHP filter_var()</a>
+            <ul>
+              <li><a target="_blank" href="http://php.net/manual/en/filter.filters.sanitize.php">Filter Sanitizers</a></li>
+              <li><a target="_blank" href="http://php.net/manual/en/filter.filters.validate.php">Filter Validators</a></li>
+            </ul>
+          </li>
+          <li><a target="_blank" href="http://php.net/manual/en/function.strip-tags.php">PHP strip_tags()</a></li>
+          <li><a target="_blank" href="http://php.net/manual/en/function.htmlspecialchars.php">PHP htmlspecialchars()</a></li>
+          <li>
+            <a target="_blank" href="http://php.net/manual/en/function.session-start.php">PHP session_start()</a>
+            <ul>
+              <li><a target="_blank" href="http://php.net/manual/en/reserved.variables.session.php">$_SESSION</a></li>
+              <li><a target="_blank" href="http://php.net/manual/en/function.session-unset.php">session_unset()</a></li>
+            </ul>
+          </li>
+          <li><a target="_blank" href="">PHP header()</a></li>
+        </ul>
+      </div>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-2.2.3.min.js" integrity="sha256-a23g1Nt4dtEYOj7bR+vTu7+T8VP13humZFBJNIYoEJo=" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+  </body>
+</html>
