1e6f2edbf0364d60f126ad0691c323eaa140aeea
Shaun McKinnon <totheroot@hotmail.ca>
2016-07-22 09:52:41 -0400

    fixed models directory
    Signed-off-by: Shaun McKinnon <totheroot@hotmail.ca>

--- lesson-10/examples/config.php
+++ lesson-10/examples/config.php
@@ -9,15 +9,17 @@
     $config['dbname'] = substr($url["path"], 1);
     $config['user'] = $url["user"];
     $config['password'] = $url["pass"];
+    $config['model_directory'] = $_SERVER['DOCUMENT_ROOT'] . '/lesson-10/examples/models';
   } else { // localhost
     $config['host'] = 'localhost';
     $config['dbname'] = 'comp-1006-lesson-examples';
     $config['user'] = 'root';
     $config['password'] = 'root';
+    $config['model_directory'] = '/lesson-10/examples/models';
   }
 
   ActiveRecord\Config::initialize( function( $cfg ) use ( $config ) {
-    $cfg->set_model_directory( $_SERVER['DOCUMENT_ROOT'] . '/lesson-10/examples/models' );
+    $cfg->set_model_directory( $config['model_directory'] );
     $cfg->set_connections( 
       array(
         'development' => "mysql://{$config['user']}:{$config['password']}@{$config['host']}/{$config['dbname']}"
