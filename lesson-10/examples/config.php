e003dbc1829f5f68cd7b7d2800f5a8ac2d347a84
Shaun McKinnon <totheroot@hotmail.ca>
2016-07-22 09:48:46 -0400

    Adjusted config for live connection
    Signed-off-by: Shaun McKinnon <totheroot@hotmail.ca>

--- lesson-10/examples/config.php
+++ lesson-10/examples/config.php
@@ -2,10 +2,19 @@
 
   require_once $_SERVER['DOCUMENT_ROOT'] . '/lesson-10/examples/vendors/php-activerecord/ActiveRecord.php';
 
-  $config['user'] = 'root';
-  $config['password'] = 'root';
-  $config['host'] = 'localhost';
-  $config['dbname'] = 'comp-1006-lesson-examples';
+  if ( preg_match('/Heroku|georgian\.shaunmckinnon\.ca/i', $_SERVER['HTTP_HOST']) ) {
+    // remote server
+    $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+    $config['host'] = $url["host"];
+    $config['dbname'] = substr($url["path"], 1);
+    $config['user'] = $url["user"];
+    $config['password'] = $url["pass"];
+  } else { // localhost
+    $config['host'] = 'localhost';
+    $config['dbname'] = 'comp-1006-lesson-examples';
+    $config['user'] = 'root';
+    $config['password'] = 'root';
+  }
 
   ActiveRecord\Config::initialize( function( $cfg ) use ( $config ) {
     $cfg->set_model_directory( $_SERVER['DOCUMENT_ROOT'] . '/lesson-10/examples/models' );
