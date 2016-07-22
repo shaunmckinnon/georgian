0fae5a2bcce142ad5f7c887d76f4c5d0ad5de456
Shaun McKinnon <totheroot@hotmail.ca>
2016-07-22 10:18:18 -0400

    fixed???
    Signed-off-by: Shaun McKinnon <totheroot@hotmail.ca>

--- lesson-10/examples/config.php
+++ lesson-10/examples/config.php
@@ -9,21 +9,20 @@
     $config['dbname'] = substr($url["path"], 1);
     $config['user'] = $url["user"];
     $config['password'] = $url["pass"];
-    $config['model_directory'] = $_SERVER['DOCUMENT_ROOT'] . '/lesson-10/examples/models';
   } else { // localhost
     $config['host'] = 'localhost';
     $config['dbname'] = 'comp-1006-lesson-examples';
     $config['user'] = 'root';
     $config['password'] = 'root';
-    $config['model_directory'] = $_SERVER['DOCUMENT_ROOT'] . '/lesson-10/examples/models';
   }
+
+  $config['model_directory'] = $_SERVER['DOCUMENT_ROOT'] . '/lesson-10/examples/models';
 
   ActiveRecord\Config::initialize( function( $cfg ) use ( $config ) {
     $cfg->set_model_directory( $config['model_directory'] );
     $cfg->set_connections( 
       array(
-        'development' => "mysql://{$config['user']}:{$config['password']}@{$config['host']}/{$config['dbname']}",
-        'production' => "mysql://{$config['user']}:{$config['password']}@{$config['host']}/{$config['dbname']}",
+        'development' => "mysql://{$config['user']}:{$config['password']}@{$config['host']}/{$config['dbname']}"
       )
     );
   });
