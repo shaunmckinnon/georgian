9b972be91ef69c53c4d2fc767aa5bd24eae78844
Shaun McKinnon <totheroot@hotmail.ca>
2016-07-25 14:18:30 -0400

    added page_title logic
    Signed-off-by: Shaun McKinnon <totheroot@hotmail.ca>

--- lesson-11/examples/includes/header.php
+++ lesson-11/examples/includes/header.php
@@ -18,7 +18,7 @@
   <head>
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.2/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-aNUYGqSUL9wG/vP7+cWZ5QOM4gsQou3sBfWRr/8S3R1Lv0rysEmnwsRKMbhiQX/O" crossorigin="anonymous">
-    <title>title</title>
+    <title><?= isset( $page_title ) ? $page_title : 'COMP-1006' ?></title>
   </head>
 
   <body>
